9885e20d35e32c7e3e3501d8fe8ea19b92539fd0
MinaW <MWRezkallah@gmail.com>
2021-08-09 13:22:37 +0200

    frontend production routing in index.js add href="/" to index.html in built angular app

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ const port = process.env.PORT || 8080;
 // parse application/json
 app.use(express.json());
 app.use(cors());
-app.get('/', (req, res) => {
-    const date = new Date();
-    const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
-    console.log("===========> Hello");
-    res.json({
-        bongs: 'BONG '.repeat(hours),
-        status: "successful deployment to gcloud!"
-    });
-});
 app.listen(port, () => console.log(`Example app listening at ${process.env.baseUrl}:${port}`));
 app.use('/api/auth', routes_1.AuthRouter);
 app.use('/api/home-slider', routes_1.HomeSliderRouter);
@@ -31,5 +22,8 @@ app.use('/api/badges', routes_1.BadgeRouter);
 app.use('/api/products', routes_1.ProductRouter);
 app.use(helmet());
 app.use(compression());
-app.use(express.static(`${process.env.multerStorage}`));
+app.use(express.static('browser'));
+app.get(/.*/, (req, res) => {
+    res.sendFile('browser/index.html',{root:__dirname});
+});
 //# sourceMappingURL=index.js.map
